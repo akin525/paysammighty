42c65d354e917bd4b888d931fe2c89c9d0798192
ACCOUNTS <akinlabisamson15@gmail.com>
2023-11-20 16:45:33 +0100

    sas

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('paylony', [\App\Http\Controllers\api\WehookController::class, 'sendwebhook'])->name('paylony');
 
 Route::group(['middleware'=> 'apikey'], function () {
 Route::post('createaccount', [\App\Http\Controllers\api\GenerateAccountController::class, 'generateaccount'])->name('createaccount');
 Route::get('allvirtual', [\App\Http\Controllers\api\FetchVirtualAccountController::class, 'getallmyaccount'])->name('allvirtual');
-Route::post('paylony', [\App\Http\Controllers\api\WehookController::class, 'sendwebhook'])->name('paylony');
 Route::post('data', [\App\Http\Controllers\Api\BillController::class, 'data'])->name('data');
 });
