a70aa427eb764475a5340534654ce5539e5eac4f
ACCOUNTS <akinlabisamson15@gmail.com>
2023-11-22 12:05:19 +0100

    sas

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ Route::group(['middleware'=> 'apikey'], function () {
 Route::post('createaccount', [\App\Http\Controllers\api\GenerateAccountController::class, 'generateaccount'])->name('createaccount');
 Route::get('allvirtual', [\App\Http\Controllers\api\FetchVirtualAccountController::class, 'getallmyaccount'])->name('allvirtual');
 Route::post('data', [\App\Http\Controllers\Api\BillController::class, 'data'])->name('data');
+Route::post('airtime', [\App\Http\Controllers\Api\AirController::class, 'airtime'])->name('airtime');
 });
