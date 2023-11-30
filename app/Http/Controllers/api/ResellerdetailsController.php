b483d206badada8266ade024fd4152b25dfd088c
PIPER <akinlabi@gmail.com>
2023-12-01 00:04:29 +0100

    pay

--- app/Http/Controllers/api/ResellerdetailsController.php
+++ app/Http/Controllers/api/ResellerdetailsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\api;
 
 use App\Models\bill_payment;
 use App\Models\Messages;
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\ResellerdetailsController;
+use App\Http\Controllers\api\ResellerdetailsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
