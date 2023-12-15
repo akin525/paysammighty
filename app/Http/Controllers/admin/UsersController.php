9258c55c4624c5024c7fc758957a0bf247c822f7
PIPER <akinlabi@gmail.com>
2023-12-15 21:13:37 +0100

    pay

--- app/Http/Controllers/admin/UsersController.php
+++ app/Http/Controllers/admin/UsersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Models\bill_payment;
 use App\Models\Business;
 use App\Models\charp;
-use App\Models\deposit;
+use App\Models\Deposit;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
