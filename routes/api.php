97dc9481c4b4dcf247b149214e9d03d31571f715
PIPER <akinlabi@gmail.com>
2024-01-16 09:03:22 +0100

    jamb

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::post('waec', [\App\Http\Controllers\api\EducationApiController::class, 'W
 Route::post('neco', [\App\Http\Controllers\api\EducationApiController::class, 'Neco'])->name('neco');
 Route::post('nabteb', [\App\Http\Controllers\api\EducationApiController::class, 'Nabteb'])->name('nabteb');
 Route::post('jamb', [\App\Http\Controllers\api\EducationApiController::class, 'Jamb'])->name('jamb');
+Route::post('verifyid', [\App\Http\Controllers\api\EducationApiController::class, 'verifyprofile'])->name('verifyid');
 Route::get('buydatacard', [\App\Http\Controllers\api\DatacardController::class, 'datacardpurchase'])->name('buydatacard');
 
 
