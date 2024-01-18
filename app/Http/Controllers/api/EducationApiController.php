846b7b1bc9b2620df540f1ced2eb95295f5a63e4
ToSHIBA <akinlabisamson15@yahoo.com>
2024-01-18 22:36:27 +0100

    club

--- app/Http/Controllers/api/EducationApiController.php
+++ app/Http/Controllers/api/EducationApiController.php
@@ -483,7 +483,7 @@ class EducationApiController
     {
         $validator = Validator::make($request->all(), [
             'profileid' => 'required',
-            'code'=>'required',
+
 
         ]);
         if ($validator->fails()) {
