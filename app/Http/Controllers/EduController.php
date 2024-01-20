493b9b1d26dd1da91938c42320999df34837ce69
ToSHIBA <akinlabisamson15@yahoo.com>
2024-01-20 23:24:06 +0100

    jamb

--- app/Http/Controllers/EduController.php
+++ app/Http/Controllers/EduController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jamb;
 use App\Models\waec;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +14,7 @@ class EduController
         $waec=waec::where('username', Auth::user()->username)->get();
         $neco=waec::where('username', Auth::user()->username)->get();
         $nabteb=waec::where('username', Auth::user()->username)->get();
-        $jamb=waec::where('username', Auth::user()->username)->get();
+        $jamb=Jamb::where('username', Auth::user()->username)->get();
 
         return view('education', compact('waec', 'neco', 'nabteb', 'jamb'));
     }
