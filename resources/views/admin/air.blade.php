4c4deeb0e4803d592193568fc6610e146eaf1df0
PIPER <akinlabi@gmail.com>
2024-01-31 23:57:37 +0100

    Airtime control

--- resources/views/admin/air.blade.php
+++ resources/views/admin/air.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.sidebar')
-@section('tittle', 'Product')
-@section('page', 'Easyaccess Product')
+@section('tittle', 'Airtime Server')
+@section('page', 'Airtime Controller')
 @section('content')
     <div class="row">
         <div class="col-lg-12">
@@ -8,14 +8,6 @@
                 <table class="table display mb-4 dataTablesCard " id="example5">
                     <thead>
                     <tr>
-                        <th>
-                            <div class="checkbox me-0 align-self-center">
-                                <div class="form-check custom-checkbox ">
-                                    <input type="checkbox" class="form-check-input" id="checkAll" required="">
-                                    <label class="form-check-label" for="checkAll"></label>
-                                </div>
-                            </div>
-                        </th>
                         <th>ID</th>
                         <th>Server</th>
                         <th>Status</th>
@@ -27,14 +19,6 @@
                     <link rel="stylesheet" href="{{asset('demo.css')}}"/>
                     @foreach($air as $deposit)
                         <tr>
-                            <td>
-                                <div class="checkbox me-0 align-self-center">
-                                    <div class="form-check custom-checkbox ">
-                                        <input type="checkbox" class="form-check-input" id="customCheckBox2" required="">
-                                        <label class="form-check-label" for="customCheckBox2"></label>
-                                    </div>
-                                </div>
-                            </td>
                             <td><span class="text-black font-w500">{{$deposit->id}}</span></td>
                             <td><span class="text-black font-w500">{{$deposit->server}}</span></td>
                             <td>
