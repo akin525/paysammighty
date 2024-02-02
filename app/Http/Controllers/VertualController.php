d1c5c7d5abcfe8b3e1400f771c384fe1d1edb564
PIPER <akinlabi@gmail.com>
2024-02-02 01:08:13 +0100

    Data Control

--- app/Http/Controllers/VertualController.php
+++ app/Http/Controllers/VertualController.php
@@ -23,47 +23,15 @@ class VertualController
     {
             $user = User::where('username', Auth::user()->username)->first();
             $business=Business::where('username', Auth::user()->username)->first();
-//            if ($user->account_prefix== null ){
-//                $msg="Kindly update your Business Profile before clicking generate virtual account";
-//                return response()->json([
-//                    'status' => 'success',
-//                    'message' => $msg,
-//                ]);
-//            }
+            if ($user->account_prefix== null ){
+                $msg="Kindly update your Business Profile before clicking generate virtual account";
+                return response()->json([
+                    'status' => 'success',
+                    'message' => $msg,
+                ]);
+            }
 
             $input=$user;
-
-        $url = 'https://api.paylony.com/api/v1/create_account';
-//        $url = 'https://app.paylony.com/api/v1/create_account';
-
-//        $headers = array(
-//            'Content-Type: application/json',
-//            'Authorization: Bearer ' . env('PAYLONY')
-//            'Authorization: Bearer sk_live_ee2k7jc4yixocqa1svajblpu9iolxkkqciquycg'
-//        );
-
-//        $data = array(
-//            "firstname" => $user['account_prefix'],
-//            "lastname" => $user['name'],
-//            "address" => "lagos nigeria",
-//            "gender" => "Male",
-//            "email" => $user['email'],
-//            "phone" => $business['phone'],
-//            "dob" => "1995-03-13",
-//            "provider" => "safehaven"
-//        );
-//
-//        $options = array(
-//            'http' => array(
-//                'header' => implode("\r\n", $headers),
-//                'method' => 'POST',
-//                'content' => json_encode($data),
-//            ),
-//        );
-//
-//        $context = stream_context_create($options);
-//        $response = file_get_contents($url, false, $context);
-
 
         $curl = curl_init();
 
