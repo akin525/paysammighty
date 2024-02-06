78d86b7a9beccfe6164c906cfadc482baa4ff3a4
PIPER <akinlabi@gmail.com>
2024-02-06 11:54:17 +0100

    insert Product

--- app/Http/Controllers/api/TransferController.php
+++ app/Http/Controllers/api/TransferController.php
@@ -132,11 +132,40 @@ class TransferController
                 ),
             );
 
-            $context = stream_context_create($options);
-            $response = file_get_contents($url, false, $context);
 
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.paylony.com/api/v1/bank_transfer',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => '{
+    "account_number":"'.$request->number.'",
+    "account_name":"'.$request->name.'",
+    "amount":"'.$request->amount.'",
+    "bank_code":"'.$request->id.'",
+    "narration":"'.$request->narration.'",
+    "reference":"'.$request->refid.'",
+    "sender_name":"'.$request->name.'"
+}',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                    'Authorization: Bearer ' . env('PAYLONY'),
+                    'Signature:'.$hash
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
             $data = json_decode($response, true);
-            return $response;
+
             $create=Withdraw::create([
                 'username'=>$user->username,
                 'amount'=>$request->amount,
